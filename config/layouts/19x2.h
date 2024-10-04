3eebf6aa2b7f01360e6e339f346c16e9711f62aa
mike barnoski <mikebarnoski@gmail.com>
2024-10-04 15:33:20 -0500

    Update 19x2.h

--- config/layouts/19x2.h
+++ config/layouts/19x2.h
@@ -30,13 +30,13 @@
   │  1   2   3   4   5             │           6   7   8   9   10  │
   │ 11  12  13  14  15             │          16  17  18  19   20  │
   │ 21  22  23  24  25             │          26  27  28  29   30  │
-  ╰───────────────╮ 31  32  33  34 │ 22 23 24 25  ╭────────────────╯
+  ╰───────────────╮ 31  32  33  34 │ 35 36 37 38  ╭────────────────╯
                   ╰────────────────┴──────────────╯
-  ╭────────────────────────────────┬───────────────────────────────╮
-  │ LT0 LT1 LT2 LT3 LT4            │          RT0 RT1 RT2 RT3 RT4  │
-  │ LM0 LM1 LM2 LM3 LM4            │          RM0 RM1 RM2 RM3 RM4  │
-  │ LB0 LB1 LB2 LB3 LB4            │          RB0 RB1 RB2 RB3 RB4  │
-  ╰───────────────╮ LH0 LH1 LH2 LH3│RH0 RH1 RH2 RH3 ╭──────────────╯ 
+  ╭────────────────────────────────┬─────────────────────────────────╮
+  │ LT0 LT1 LT2 LT3 LT4            │            RT0 RT1 RT2 RT3 RT4  │
+  │ LM0 LM1 LM2 LM3 LM4            │            RM0 RM1 RM2 RM3 RM4  │
+  │ LB0 LB1 LB2 LB3 LB4            │            RB0 RB1 RB2 RB3 RB4  │
+  ╰───────────────╮ LH0 LH1 LH2 LH3│RH0 RH1 RH2 RH3 ╭────────────────╯ 
                   ╰────────────────┴────────────────╯
 */
 
@@ -72,25 +72,33 @@
 #define LB3 24
 #define LB4 25
 
-#define RB1 23  // right-bottom row
-#define RB2 24
-#define RB3 25
+#define RB1 26  // right-bottom row
+#define RB2 27
+#define RB3 28
+#define RB4 29
+#define RB5 30
 
-#define LH0 26  // left thumb key
+#define LH0 31  // left thumb key
+#define LH1 32 
+#define LH2 33 
+#define LH3 34 
 
-#define RH0 27  // right thumb key
+#define RH0 35  // right thumb key
+#define RH1 36
+#define RH2 37
+#define RH3 38
 
-#define KEYS_L LT0 LT1 LT2 LT3 LT4 LM0 LM1 LM2 LM3 LM4     LB1 LB2 LB3      // left-hand keys
-#define KEYS_R RT0 RT1 RT2 RT3 RT4 RM0 RM1 RM2 RM3 RM4     RB1 RB2 RB3      // right-hand keys
-#define THUMBS         LH0 RH0                                              // thumb keys
+#define KEYS_L LT0 LT1 LT2 LT3 LT4 LM0 LM1 LM2 LM3 LM4 LB0 LB1 LB2 LB3 LB4     // left-hand keys
+#define KEYS_R RT0 RT1 RT2 RT3 RT4 RM0 RM1 RM2 RM3 RM4 RB0 RB1 RB2 RB3 RB4     // right-hand keys
+#define THUMBS         LH0 LH1 LH2 LH3 RH0 RH1 RH2 RH3                         // thumb keys
 
-#define LAYER_FROM36( \
-    k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, \
-    k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, \
-    k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, \
-              k30, k31, k32, k33, k34, k35            \
+#define LAYER_19x2( \
+    k00, k01, k02, k03, k04,                                    k05, k06, k07, k08, k09, \
+    k10, k11, k12, k13, k14,                                    k15, k16, k17, k18, k19, \
+    k20, k21, k22, k23, k24,                                    k25, k26, k27, k28, k29, \
+                        k30, k31, k32, k33,      k34, k35, k36, k37                      \
 ) \
-    k00  k01  k02  k03  k04  k05  k06  k07  k08  k09  \
-    k10  k11  k12  k13  k14  k15  k16  k17  k18  k19  \
-         k21  k22  k23            k26  k27  k28       \
-                        k32  k33
+    k00  k01  k02  k03  k04                                     k05  k06  k07  k08  k09  \
+    k10  k11  k12  k13  k14                                     k15  k16  k17  k18  k19  \
+    k20  k21  k22  k23  k24                                     k25  k26  k27  k28  k29  \
+                        k30  k31  k32  k33       k34  k35  k36  k37
