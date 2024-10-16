6a56b5880338414cd17220670db22d5ecc4a6cfc
mike barnoski <mikebarnoski@gmail.com>
2024-10-16 08:48:04 -0500

    Update 19x2_13x2.h

--- config/keypos_def/19x2_13x2.h
+++ config/keypos_def/19x2_13x2.h
@@ -94,9 +94,9 @@
 #define THUMBS     LH0 LH1 LH2 LH3   RH0 RH1 RH2 RH3         // thumb keys
 
 #define LAYER_FROM38( \
-              k02, k03, k04,                                    k05, k06, k07,  \
-              k12, k13, k14,                                    k15, k16, k17,  \
-              k22, k23, k24,                                    k25, k26, k27,  \
+    k00, k01, k02, k03, k04,                                    k05, k06, k07, k08, k09, \
+    k10, k11, k12, k13, k14,                                    k15, k16, k17, k18, k19, \
+    k20, k21, k22, k23, k24,                                    k25, k26, k27, k28, k29, \
                         k30, k31, k32, k33,      k34, k35, k36, k37                      \
 ) \
               k02  k03  k04                                     k05  k06  k07  \
